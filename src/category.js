cab7aba14194aa90d711a2c3c19065f692a46b76
fraolb <Fraolbereket@gmail.com>
2023-05-10 12:21:46 +0300

    customer list crud

--- src/category.js
+++ src/category.js
@@ -1,6 +1,9 @@
 var category = function category(key) {
   
   // Write the Logic here
+  let value = key == "pl" ? "Personal Loan" : key == "Vl" ? "Vehicle Loan" : key == "EL" ? "Education Loan" : "Home Loan"
+ 
+  return value
 
   };
   
--- src/customer.js
+++ src/customer.js
@@ -1,15 +1,57 @@
-var customerList=[];
+var customerList = [];
+const { category } = require("./category");
 
-const addCustomer=(CustomerId, CustomerName,CustomerAge,CustomerAddress,CustomerContactNumber,Category)=>{
-      // Write the Logic here
-}
-
-const updateCustomer=(CustomerId, CustomerName,CustomerAge,CustomerAddress,CustomerContactNumber,Category)=>{
-      // Write the Logic here
-}
-
-const getAllCustomers=()=>{
+const addCustomer = (
+  CustomerId,
+  CustomerName,
+  CustomerAge,
+  CustomerAddress,
+  CustomerContactNumber,
+  Category
+) => {
   // Write the Logic here
-}
+  customerList.filter((i) => i[0] == CustomerId).length == 0
+    ? (customerList = [
+        ...customerList,
+        [
+          CustomerId,
+          CustomerName,
+          CustomerAge,
+          CustomerAddress,
+          CustomerContactNumber,
+          category(Category),
+        ],
+      ])
+    : null;
+};
 
-module.exports={addCustomer,updateCustomer,getAllCustomers}
+const updateCustomer = (
+  CustomerId,
+  CustomerName,
+  CustomerAge,
+  CustomerAddress,
+  CustomerContactNumber,
+  Category
+) => {
+  // Write the Logic here
+  let index = customerList.findIndex((i) => i[0] == CustomerId)
+  customerList[index][1] = CustomerName;
+  customerList[index][2] = CustomerAge;
+  customerList[index][3] = CustomerAddress;
+  customerList[index][4] = CustomerContactNumber;
+  customerList[index][5] = category(Category);
+};
+
+const getAllCustomers = () => {
+  // Write the Logic here
+  return customerList;
+};
+
+module.exports = { addCustomer, updateCustomer, getAllCustomers };
+
+
+// addCustomer("c001","Nina Sen",24,"Kolkata,West Bengal",910855748,category("EL"));
+// console.log(customerList)
+
+// updateCustomer("c001","Nina Dobrev",24,"Kolkata,West Bengal",910855748,category("EL"));
+// console.log(customerList)
